a3aa904d0cba7ec4c40af09d6dea2b5d946aacda
Varga György Márk <gyorgy.varga@shiwaforce.com>
2023-11-05 20:08:35 +0100

    challange soltuion

--- src/App.vue
+++ src/App.vue
@@ -111,7 +111,7 @@ const validation = reactive({
   passwordConfirmation: { valid: true, message: '' },
 });
 
-const validateEmail = (email) => {
+const validateEmail = (email: string) => {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email.toLowerCase());
 };
