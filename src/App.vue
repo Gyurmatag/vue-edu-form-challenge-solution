9eb1cf6842e135c26069084d7b2c5449083000a1
Varga György Márk <gyorgy.varga@shiwaforce.com>
2023-11-13 23:23:57 +0100

    javitas

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
         </h2>
       </div>
       <form class="mt-8 space-y-6" @submit.prevent="handleSubmit">
-        <div class="flex flex-col space-y-3 rounded-md shadow-sm -space-y-px">
+        <div class="flex flex-col rounded-md shadow-sm -space-y-px">
           <div>
             <label for="email-address" class="block text-sm font-medium text-gray-700">Email address</label>
             <input
@@ -92,80 +92,68 @@ const form = reactive({
 });
 
 const validation = reactive({
-  email: { valid: false, message: '' },
-  name: { valid: false, message: '' },
-  password: { valid: false, message: '' },
-  passwordConfirmation: { valid: false, message: '' },
+  email: { message: '' },
+  name: { message: '' },
+  password: { message: '' },
+  passwordConfirmation: { message: '' },
 });
 
 const emailRegex = new RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
 const validateEmailField = () => {
   if (!form.email) {
-    validation.email.valid = false;
     validation.email.message = 'Email is required';
   } else if (!emailRegex.test(form.email)) {
-    validation.email.valid = false;
     validation.email.message = 'Invalid email address';
   } else {
-    validation.email.valid = true;
     validation.email.message = '';
   }
 };
 
 const validateNameField = () => {
   if (!form.name) {
-    validation.name.valid = false;
     validation.name.message = 'Name is required';
   } else if (form.name.length < 5) {
-    validation.name.valid = false;
     validation.name.message = 'Name must be at least 5 characters';
   } else if (form.name.length > 250) {
-    validation.name.valid = false;
     validation.name.message = 'Name must not exceed 250 characters';
   } else {
-    validation.name.valid = true;
     validation.name.message = '';
   }
 };
 
 const validatePasswordField = () => {
   if (!form.password) {
-    validation.password.valid = false;
     validation.password.message = 'Password is required';
   } else if (form.password.length < 8) {
-    validation.password.valid = false;
     validation.password.message = 'Password must be at least 8 characters';
   } else {
-    validation.password.valid = true;
     validation.password.message = '';
   }
 };
 
 const validatePasswordConfirmationField = () => {
   if (!form.passwordConfirmation) {
-    validation.passwordConfirmation.valid = false;
     validation.passwordConfirmation.message = 'Password confirmation is required';
   } else if (form.password !== form.passwordConfirmation) {
-    validation.passwordConfirmation.valid = false;
     validation.passwordConfirmation.message = 'Passwords do not match';
   } else {
-    validation.passwordConfirmation.valid = true;
     validation.passwordConfirmation.message = '';
   }
 };
 
 const isValid = computed(() => {
   return (
-      form.email.trim() !== '' &&
-      form.name.trim() !== '' &&
-      form.password.trim() !== '' &&
-      form.passwordConfirmation.trim() !== '' &&
-      validation.email.valid &&
-      validation.name.valid &&
-      validation.password.valid &&
-      validation.passwordConfirmation.valid
+      form.email.length > 0 &&
+      form.name.length > 0 &&
+      form.password.length > 0 &&
+      form.passwordConfirmation.length > 0 &&
+      !validation.email.message &&
+      !validation.name.message &&
+      !validation.password.message &&
+      !validation.passwordConfirmation.message
   );
 });
+
 
 const handleSubmit = () => {
   if (isValid.value) {
